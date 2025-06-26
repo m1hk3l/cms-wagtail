f95a8d8d09965f3564c3418da325f3d0ad13d8cb
Mihkel Kagovere <mihkelk.kagovere@ria.ee>
2025-06-26 21:32:11 +0300

    feat: Added child pages to blog

--- wcms/blog/models.py
+++ wcms/blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
+from modelcluster.fields import ParentalKey
 
 # add this:
 from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
-
+    # add the get_context method:
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
     content_panels = Page.content_panels + ["intro"]
 
 class BlogPage(Page):
@@ -20,4 +27,19 @@ class BlogPage(Page):
         index.SearchField('body'),
     ]
 
-    content_panels = Page.content_panels + ["date", "intro", "body"]
+    content_panels = Page.content_panels + [
+        "date", "intro", "body",
+
+        # Add this
+         "gallery_images",
+        ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = ["image", "caption"]
