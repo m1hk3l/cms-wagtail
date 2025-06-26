70fea828b03747ca58f3bbef8afa156019bb052c
Mihkel Kagovere <mihkelk.kagovere@ria.ee>
2025-06-26 21:43:02 +0300

    feat: added gallery

--- wcms/blog/models.py
+++ wcms/blog/models.py
@@ -21,18 +21,20 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    # Add the main_image method:
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
-    content_panels = Page.content_panels + [
-        "date", "intro", "body",
-
-        # Add this
-         "gallery_images",
-        ]
+    content_panels = Page.content_panels + ["date", "intro", "body", "gallery_images"]
 
 
 class BlogPageGalleryImage(Orderable):
