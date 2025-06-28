f0f4846fed71c3c780874161b6e5270f7b638b1c
m1hk3l <mihkel.kagovere@gmail.com>
2025-06-28 21:53:07 +0300

    test: browser based test with selenium

---
+++ wcms/tests/test_connection.sh
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = False
+options.binary_location = "/Users/mihkelk/.local/bin/firefox-developer"
+
+service = Service()
+browser = webdriver.Firefox(service=service, options=options)
+
+browser.get("http://localhost:8000")
+
+print(browser.title)
+assert "Congratulations!" in browser.title
+print("OK")
+
+browser.quit()
