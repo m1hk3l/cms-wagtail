fe2e97c0c677f795d6f2a5e9577922c090d5c6e7
m1hk3l <mihkel.kagovere@gmail.com>
2025-06-29 13:39:18 +0300

    Finalized footer and header

--- wcms/frontend/src/main.tsx
+++ wcms/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import '@/index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Navbar } from './components/Navbar';
+import { Footer } from '@/components/Footer'
 
 document.addEventListener('DOMContentLoaded', () => {
   const navbarEl = document.getElementById('navbar');
@@ -14,4 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     console.error("Navbar element (#navbar) not found on DOMContentLoaded");
   }
+   const footerEl = document.getElementById('footer');
+  if (footerEl) {
+    ReactDOM.createRoot(footerEl).render(
+      <React.StrictMode>
+        <Footer />
+      </React.StrictMode>,
+    );
+  }
 });
+
