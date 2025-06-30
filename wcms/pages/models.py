db0d643217b4864457b2d6593d8c2afa99e9359d
m1hk3l <mihkel.kagovere@gmail.com>
2025-06-30 03:07:06 +0300

    feat: added more styling features to flex page

--- wcms/pages/models.py
+++ wcms/pages/models.py
@@ -7,7 +7,7 @@ from wagtail.images.blocks import ImageChooserBlock
 class FlexibleContentPage(Page):
     parent_page_types = ['home.HomePage']  # or 'wagtailcore.Page' to allow anywhere
     body = StreamField([
-        ('text_block', blocks.RichTextBlock(features=['h2', 'h3', 'bold', 'italic', 'link'])),
+        ('text_block', blocks.RichTextBlock(features=['h1', 'h2', 'h3', 'bold', 'italic', 'link'])),
         ('image_block', ImageChooserBlock()),
         ('image_text_block', blocks.StructBlock([
             ('image', ImageChooserBlock(required=True)),
