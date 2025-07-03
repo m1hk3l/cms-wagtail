0c61dfa219c92d13c25c063beab29d7eedd1500b
m1hk3l <mihkel.kagovere@gmail.com>
2025-07-03 08:14:02 +0300

    breaking_error: Project transfer feature is missing

---
+++ akvarellistuudio/akvarellistuudio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!k7z28@ieuk0qo)x^=ncbo!7&3wo8dugn&z28^9ab^i28bhm04"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
